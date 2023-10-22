a6525f2f8d77fe137a9e5bcad64bc5f228683ab6
Roland Kajatin <roland.kajatin@gmail.com>
2023-10-22 09:26:23 +0200

    Initial Commit

---
+++ Padel Pro Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Padel Pro Watch App
+//
+//  Created by Roland Kajatin on 22/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Padel Pro Watch App/Padel_ProApp.swift
@@ -0,0 +1,17 @@
+//
+//  Padel_ProApp.swift
+//  Padel Pro Watch App
+//
+//  Created by Roland Kajatin on 22/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Padel_Pro_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
